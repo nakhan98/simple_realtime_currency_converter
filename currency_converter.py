9aba624ca5718d752f9a64f35c9f00064dd50827
Nasef Khan <nasef@Asus-Debian>
2015-03-02 11:10:48 +0000

    - Removed tuple of array codes to check against - Assuming if api returns a value of '0.00' as exchange rate, an   incorrect currency code was entered.

--- currency_converter.py
+++ currency_converter.py
@@ -12,26 +12,6 @@ import urllib2
 
 api_url = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=%s%s=X"
 
-# Active currency codes - http://en.wikipedia.org/wiki/ISO_4217#Active_codes
-# Retrieved - 27th February
-currency_codes = ("AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG",
-"AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BOV",
-"BRL", "BSD", "BTN", "BWP", "BYR", "BZD", "CAD", "CDF", "CHE", "CHF", "CHW",
-"CLF", "CLP", "CNY", "COP", "COU", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF",
-"DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL",
-"GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
-"IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KGS",
-"KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD",
-"LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR",
-"MVR", "MWK", "MXN", "MXV", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR",
-"NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON",
-"RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL",
-"SOS", "SRD", "SSP", "STD", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP",
-"TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "USN", "USS", "UYI", "UYU",
-"UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XBA", "XBB", "XBC",
-"XBD", "XCD", "XDR", "XFU", "XOF", "XPD", "XPF", "XPT", "XSU", "XTS", "XUA",
-"XXX", "YER", "ZAR", "ZMW", "ZWL") 
-
 instructions = '''Error... python currency_converter.py amount [source currency code] [target currency code]
 Example: python currency_converter.py 15 gbp usd'''
 
@@ -39,11 +19,13 @@ def get_latest_rates(source_currency, target_currency):
     query_url = api_url % (source_currency, target_currency)
     currency_csv = urllib2.urlopen(query_url).read()
     currency_rate = currency_csv.split(",")[1]
+    if currency_rate == "0.00":
+        print("Error...Please check your currency codes")
+        sys.exit(3)
     return float(currency_rate)
 
 
 def main():
-    #ipdb.set_trace()
     if len(sys.argv) != 4:
         if len(sys.argv) == 1:
             print(__doc__)
@@ -51,13 +33,9 @@ def main():
         else:
             print(instructions)
             sys.exit(1)
-    elif (sys.argv[2].upper() not in currency_codes or 
-            sys.argv[3].upper() not in currency_codes):
-        print(instructions)
-        sys.exit(2)
     elif (sys.argv[2].upper() == sys.argv[3].upper()):
         print("Your source and target currencies are the same")
-        sys.exit(3)
+        sys.exit(2)
 
     amount, source_currency, target_currency = (sys.argv[1], sys.argv[2].upper(), sys.argv[3].upper())
     
